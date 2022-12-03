f38dc60a462c4ae661786b192138b027b53450a7
baeksbs3 <baeksbs3@gmail.com>
2022-12-03 22:00:38 +0900

    error fixed

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const app = express();
 const todosRouter = require("./routes/todosRouter.js");
+const app = express();
 const dotenv = require("dotenv");
 require("dotenv").config();
 const connect = require("./models/index.js");
 connect();
-
-app.use("/api", express.json(), todosRouter);
+app.use(express.json());
+app.use("/api", todosRouter);
 app.use(express.static("./assets"));
 
-app.listen(8080, () => {
+app.listen(4010, () => {
   console.log("서버가 켜졌어요!");
 });
--- routes/todosRouter.js
+++ routes/todosRouter.js
@@ -1,19 +1,30 @@
 const express = require("express");
-const Todo = require("../models/todo.js");
 const router = express.Router();
+const Todo = require("../models/todo.js");
 
 router.get("/", (req, res) => {
   res.send("Hi!");
 });
 
-router.post("/todos", async (res, req) => {
+router.post("/todos", async (req, res) => {
+  console.log(req.body);
   const { value } = req.body;
-  const maxOrderByUserId = await Todo.findOne().sort("-orders").exec();
+  try {
+    //check if there is orderId first, if there is add 1, not make it 1
+    const maxOrderByUserId = await Todo.findOne().sort("-orders").exec();
+    const order = maxOrderByUserId ? maxOrderByUserId.order + 1 : 1;
 
-  const order = maxOrderByUserId ? maxOrderByUserId.order + 1 : 1;
+    //now, create todo value and orderid in the collection
+    const todo = new Todo({ value, order });
+    await todo.save();
+    res.send({ todo });
+  } catch (error) {
+    res.status(400).json({ error: "error" });
+  }
+});
 
-  const todo = new Todo({ value, order });
-  await todo.save();
-  res.send({ todo });
+router.get("/todos", async (req, res) => {
+  const todos = await Todo.find().sort("-order").exec();
+  res.json({ todos: todos });
 });
 module.exports = router;
